260f7cc317288612037322a9d53e2cec891d204a
arhor <RZAB9qkn>
2020-08-05 12:27:35 +0300

    shared lib build script migrated to kts format;

---
+++ diploma-shared/build.gradle.kts
@@ -0,0 +1,43 @@
+plugins {
+    id("org.jetbrains.kotlin.multiplatform")
+}
+
+group = "org.arhor"
+version = "0.0.1-SNAPSHOT"
+description = "diploma-shared"
+
+kotlin {
+
+    jvm {
+        val main by compilations.getting {
+            kotlinOptions {
+                jvmTarget = JavaVersion.VERSION_11.toString()
+            }
+        }
+    }
+
+    js {
+        val main by compilations.getting {
+            kotlinOptions {
+                metaInfo = false
+                sourceMap = false
+                moduleKind = "commonjs"
+                outputFile = "${project.buildDir}/compiled-js/${project.name}.js"
+            }
+        }
+    }
+
+    sourceSets {
+        commonMain {
+            dependencies {
+                implementation("org.jetbrains.kotlin:kotlin-stdlib-common")
+            }
+        }
+        commonTest {
+            dependencies {
+                implementation("org.jetbrains.kotlin:kotlin-test-common")
+                implementation("org.jetbrains.kotlin:kotlin-test-annotations-common")
+            }
+        }
+    }
+}
