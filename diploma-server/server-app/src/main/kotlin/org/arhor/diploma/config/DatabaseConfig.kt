325f727e359c94e6a2610ccf83e495d70b00ab71
Maksim Buryshynets <arhor18@gmail.com>
2021-04-25 16:59:03 +0300

    connection factory fix;

--- diploma-server/server-app/src/main/kotlin/org/arhor/diploma/config/DatabaseConfig.kt
+++ diploma-server/server-app/src/main/kotlin/org/arhor/diploma/config/DatabaseConfig.kt
@@ -1,8 +1,5 @@
 package org.arhor.diploma.config
 
-import io.r2dbc.spi.ConnectionFactories
-import io.r2dbc.spi.ConnectionFactory
-import mu.KotlinLogging
 import org.arhor.diploma.util.SpringProfile
 import org.springframework.boot.autoconfigure.r2dbc.R2dbcProperties
 import org.springframework.context.annotation.Bean
@@ -13,25 +10,14 @@ import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories
 import org.springframework.transaction.annotation.EnableTransactionManagement
 import java.net.URI
 
-val log = KotlinLogging.logger {}
-
 @Configuration
 @EnableR2dbcRepositories("org.arhor.diploma")
 @EnableTransactionManagement
 class DatabaseConfig {
 
-//    @Bean
-//    @Profile("!${SpringProfile.DEVELOPMENT}")
-//    fun connectionFactory(): ConnectionFactory {
-//        val uri = URI(System.getenv("DATABASE_URL"))
-//
-//        val (username, password) = uri.userInfo.split(":").toTypedArray()
-//
-//        return ConnectionFactories.get("r2dbcs:postgres://${username}:${password}@${uri.host}:${uri.port}/${uri.path}")
-//    }
-
     @Bean
     @Primary
+    @Profile(SpringProfile.PRODUCTION)
     fun r2dbcProperties(): R2dbcProperties {
         val uri = URI(System.getenv("DATABASE_URL"))
 
