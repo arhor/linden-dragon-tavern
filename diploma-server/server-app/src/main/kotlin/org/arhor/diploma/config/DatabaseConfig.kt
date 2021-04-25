c1ca784029485036839f4b4894adce103ba16f25
Maksim Buryshynets <arhor18@gmail.com>
2021-04-25 17:54:43 +0300

    more playing with props;

--- diploma-server/server-app/src/main/kotlin/org/arhor/diploma/config/DatabaseConfig.kt
+++ diploma-server/server-app/src/main/kotlin/org/arhor/diploma/config/DatabaseConfig.kt
@@ -27,7 +27,7 @@ class DatabaseConfig {
             this.url = "r2dbc:postgres://${uri.host}:${uri.port}/${uri.path}"
             this.username = username
             this.password = password
-//            this.properties["sslmode"] = "require"
+            this.properties["sslMode"] = "require"
             this.properties["ssl"] = "true"
         }
     }
