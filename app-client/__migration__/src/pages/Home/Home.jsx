514f733e5920bcf5244a82454f4a2a6a88fb5819
Maksim Buryshynets <arhor18@gmail.com>
2022-05-07 23:25:43 +0300

    PropTypes removed from production build (removed redundancies);

--- app-client/__migration__/src/pages/Home/Home.jsx
+++ app-client/__migration__/src/pages/Home/Home.jsx
@@ -1,14 +1,10 @@
 import React from 'react';
 
-import MainAbilityCounter, { ABILITY } from '@/components/MainAbilityCounter';
 import StatelessWidget, { SIZE, TYPE } from '@/components/StatelessWidget';
 
 function Home() {
     return (
-        <>
-            <StatelessWidget type={TYPE.PAGE} size={SIZE.LARGE} title="Home page"/>
-            <MainAbilityCounter  value={20} name={ABILITY.CHA}/>
-        </>
+        <StatelessWidget type={TYPE.PAGE} size={SIZE.LARGE} title="Home page"/>
     );
 }
 
