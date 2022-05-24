3245277f23f44de2215993f8e4a4629136d23a8d
Maksim Buryshynets <arhor18@gmail.com>
2022-05-24 17:12:29 +0300

    vite config improved;

--- app-client/vite.config.ts
+++ app-client/vite.config.ts
@@ -1,28 +1,37 @@
-import { URL, fileURLToPath } from 'url';
+import { fileURLToPath, URL } from 'url';
 
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import eslint from 'vite-plugin-eslint';
 
-export default defineConfig({
-    plugins: [react(), eslint()],
-    resolve: {
-        alias: {
-            '@': fileURLToPath(new URL('src', import.meta.url)),
-        },
-    },
-    server: {
-        proxy: {
-            '^/api': {
-                target: process.env.API_BASE_URL,
-                changeOrigin: true,
+export default defineConfig(({ mode }) => {
+    const resolvePath = (path: string) => fileURLToPath(new URL(path, import.meta.url));
+
+    const rootProjectDir = resolvePath('..');
+    const variablePrefixes = [''];
+
+    process.env = { ...loadEnv(mode, rootProjectDir, variablePrefixes) };
+
+    return {
+        plugins: [react(), eslint()],
+        resolve: {
+            alias: {
+                '@': resolvePath('src'),
             },
         },
-    },
-    test: {
-        globals: true,
-        environment: 'jsdom',
-        watch: false,
-        setupFiles: ['src/tests.setup.ts'],
-    },
+        server: {
+            proxy: {
+                '^/api': {
+                    target: process.env.API_BASE_URL,
+                    changeOrigin: true,
+                },
+            },
+        },
+        test: {
+            globals: true,
+            environment: 'jsdom',
+            watch: false,
+            setupFiles: ['src/tests.setup.ts'],
+        },
+    }
 });
