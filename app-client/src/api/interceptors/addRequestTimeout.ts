80aa76ae133ea4a2048f3156465935024f4baddd
Maksim Buryshynets <arhor18@gmail.com>
2022-05-27 12:07:35 +0300

    build scripts refactoring;

--- app-client/src/api/interceptors/addRequestTimeout.ts
+++ app-client/src/api/interceptors/addRequestTimeout.ts
@@ -1,17 +1,16 @@
 import { AxiosRequestConfig } from 'axios';
 import log from 'loglevel';
 
-import { Optional } from '@/utils/core-utils';
-
 const DEFAULT_TIMEOUT = 10_000;
 
-const timeouts: ReadonlyMap<Optional<string>, number> = new Map([
-    ['v1/creatures', 1_000],
+const timeouts: ReadonlyMap<string, number> = new Map([
+    ['api/v1/creatures', 1_000],
 ]);
 
 export default function addRequestTimeout(config: AxiosRequestConfig): AxiosRequestConfig {
     const { timeout, ...restConfig } = config;
-    const timeoutToUse = timeout || timeouts.get(config.url) || DEFAULT_TIMEOUT;
+    const requestUrl = config.baseURL + '/' + config.url;
+    const timeoutToUse = timeout || timeouts.get(requestUrl) || DEFAULT_TIMEOUT;
 
     log.debug('Using request timeout %s for the url: %s/%s', timeoutToUse, config.baseURL, config.url);
 
